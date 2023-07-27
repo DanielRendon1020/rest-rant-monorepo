a221e0dba41cbd9010f7bf45850fc0e5ffd44267
DanielRendon1020 <d.danny1020@gmail.com>
2023-07-26 20:48:01 -0700

    cookie session

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -8,6 +8,7 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({
     where: { email: req.body.email },
   });
+
   if (
     !user ||
     !(await bcrypt.compare(req.body.password, user.passwordDigest))
@@ -16,7 +17,22 @@ router.post("/", async (req, res) => {
       message: `Could not find a user with the provided username and password`,
     });
   } else {
+    req.session.userId = user.userId;
     res.json({ user });
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  console.log(req.session.userId);
+  try {
+    let user = await User.findOne({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+    res.json(user);
+  } catch {
+    res.json(null);
   }
 });
 
