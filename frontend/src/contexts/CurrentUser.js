e7b45bbf0dcb483c6ff966233008e27a28968e01
DanielRendon1020 <d.danny1020@gmail.com>
2023-07-27 21:28:55 -0700

    fix on header

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,14 +3,14 @@ import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
                 headers: {
-                    'Authorization': `Bearer  + ${localStorage.getItem('token')}`
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
                 }
             })
             let user = await response.json()
